e812f897b02db8e01a8e54a5e95bb63d3b6e13cc
Fabien Chuc <fabienchuc@pc11.home>
2018-02-01 01:43:26 +0100

    Router

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Front Controller
+ */
+
+//echo 'Requested URL = "'. $_SERVER['QUERY_STRING'] .'"';
+
+/*
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+echo get_class($router);
